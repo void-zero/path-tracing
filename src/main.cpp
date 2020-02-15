689c4c545c3a8b58b90e2a24ca996ed6ece67663
void-zero <wallace.lucena@hotmail.com>
2020-02-14 21:19:40 -0300

    Added info.txt

--- src/main.cpp
+++ src/main.cpp
@@ -3,11 +3,13 @@
 #include "include/scene.h"
 #include "include/buffer.h"
 #include "include/raytracer.h"
+#include <chrono>
 
 int main( void )
 {
     unsigned int x_resolution = 512;
     unsigned int y_resolution = 512;
+    std::chrono::high_resolution_clock::time_point t0, t1; // Time points used to measure the render time
 
     OrthographicCamera camera{ -1.25f, 
                                 1.25f, 
@@ -19,6 +21,8 @@ int main( void )
                                 glm::vec3{ 0.0f, 0.0f, -1.0f } };   // look at
     Scene scene{};
     
+    t0 = std::chrono::high_resolution_clock::now();
+
     scene.load();
 
     Buffer rendering_buffer{ x_resolution, y_resolution };
@@ -32,8 +36,12 @@ int main( void )
 
     rt.integrate(); // Renders the final image.
 
+    t1 = std::chrono::high_resolution_clock::now();
+
+    std::cout << "\nRender time: " << (double) std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count()/1000000000 << " (s)\n";
+
     // Save the rendered image to a .ppm file.
-    rendering_buffer.save( "renders/output_image.ppm" );
+    rendering_buffer.save( "renders/Assignment 2/triangle.ppm" );
 
     return EXIT_SUCCESS;
 }
